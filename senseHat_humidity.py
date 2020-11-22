ec75ae2639410639a0f2807f2298ad404edf096f
Loïc VENTRE <loic@ventre.tel>
2020-11-22 19:26:07 +0100

    Add files via upload

---
+++ senseHat_humidity.py
@@ -0,0 +1,109 @@
+from sense_emu import SenseHat
+import time
+
+sense = SenseHat()
+sense.low_light = True
+#sense.set_rotation(270.0)
+
+# red
+r = (255, 0, 0)
+
+# pink
+p = (204, 0, 204)
+
+# orange
+o = (255, 128, 0)
+
+# yellow
+y = (255, 255, 0)
+
+# green
+g = (0, 255, 0)
+
+# aqua
+a = (0, 255, 255)
+
+# blue
+b = (0, 0, 255)
+
+# purple
+pr = (128, 0, 255)
+
+# white
+w = (255, 255, 255)
+
+# empty (no color)
+e = (0, 0, 0)
+
+red_heart = [
+e, e, e, e, e, e, e, e,
+e, r, r, e, r, r, e, e,
+r, r, r, r, r, r, r, e,
+r, r, r, r, r, r, r, e,
+r, r, r, r, r, r, r, e,
+e, r, r, r, r, r, e, e,
+e, e, r, r, r, e, e, e,
+e, e, e, r, e, e, e, e
+]
+
+pink_heart = [
+e, e, e, e, e, e, e, e,
+e, p, p, e, p, p, e, e,
+p, p, p, p, p, p, p, e,
+p, p, p, p, p, p, p, e,
+p, p, p, p, p, p, p, e,
+e, p, p, p, p, p, e, e,
+e, e, p, p, p, e, e, e,
+e, e, e, p, e, e, e, e
+]
+
+orange_heart = [
+e, e, e, e, e, e, e, e,
+e, o, o, e, o, o, e, e,
+o, o, o, o, o, o, o, e,
+o, o, o, o, o, o, o, e,
+o, o, o, o, o, o, o, e,
+e, o, o, o, o, o, e, e,
+e, e, o, o, o, e, e, e,
+e, e, e, o, e, e, e, e
+]
+
+heart_colors = [red_heart, pink_heart, orange_heart]
+def rainbow_hearts():
+  for color in heart_colors:
+
+    sense.set_pixels(color)
+    time.sleep(0.5)
+
+    # Clear the LED display
+    sense.clear()
+
+sense.clear()
+sense.show_message("Bonjour de Alice et Charles!", text_colour=y, back_colour=r, scroll_speed=0.05)
+
+temperature = round(sense.get_temperature(), 1)
+message = "Temperature is " + str(temperature) + " C and "
+sense.show_message(message, text_colour=o, back_colour=b, scroll_speed=0.05)
+
+humidity = round(sense.get_humidity(), 1)
+message = "humidity is " + str(humidity) + "%"
+sense.show_message(message, text_colour=y, back_colour=b, scroll_speed=0.05)
+
+time.sleep(1)
+
+smiley = [
+e, e, e, e, e, e, e, e,
+e, b, b, e, b, b, e, e,
+e, b, b, e, b, b, e, e,
+e, e, e, p, e, e, e, e,
+e, p, e, p, e, p, e, e,
+e, p, e, e, e, p, e, e,
+e, e, p, p, p, e, e, e,
+e, e, e, e, e, e, e, e
+]
+
+sense.set_pixels(smiley)
+time.sleep(1)
+
+rainbow_hearts()
+time.sleep(1)
